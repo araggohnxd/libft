4c1ac82fe6a34f456bc6d16bb45ca6abcddb4c07
araggohnxd <matheusnagato300@gmail.com>
2022-04-01 18:35:13 -0300

    (Mandatory) ft_strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 18:12:06 by maolivei          #+#    #+#             */
+/*   Updated: 2022/04/01 18:34:56 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*last;
+
+	last = NULL;
+	while (*s)
+	{
+		if (*s == c)
+			last = (char *) s;
+		s++;
+	}
+	if (last == NULL)
+		return (NULL);
+	return (last);
+}
