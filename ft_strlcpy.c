f732130af93b7279dd76989875d3e310f1120b95
araggohnxd <matheusnagato300@gmail.com>
2022-03-31 23:09:15 -0300

    (Mandatory) ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 22:55:56 by maolivei          #+#    #+#             */
+/*   Updated: 2022/03/31 23:05:35 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	strlcpy(char *dst, const char *src, size_t size)
+{
+	int	i;
+	int	src_len;
+
+	i = 0;
+	src_len = ft_strlen(src);
+	while (i < src_len && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_len);
+}
