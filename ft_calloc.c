ce171d06fda5f94c50e9721e145381c6e2044584
araggohnxd <matheusnagato300@gmail.com>
2022-04-04 00:04:48 -0300

    (Mandatory) ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/03 23:42:53 by maolivei          #+#    #+#             */
+/*   Updated: 2022/04/04 00:03:00 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = NULL;
+	if (nmemb * size > INT_MAX)
+		return (ptr);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
