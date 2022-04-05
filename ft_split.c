87cf4bce3d113e5572bf683a88918b35f1c63607
araggohnxd <matheusnagato300@gmail.com>
2022-04-05 11:09:41 -0300

    (Mandatory) ft_split

---
+++ ft_split.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/04 13:08:22 by maolivei          #+#    #+#             */
+/*   Updated: 2022/04/05 11:04:50 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+static void	ft_fill_strings(const char *s, char c, char **split, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (s[i] && j < size)
+	{
+		k = 0;
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+			split[j][k++] = s[i++];
+		split[j][k] = '\0';
+		j++;
+	}
+	k = 0;
+	split[j] = NULL;
+}
+
+static void	ft_malloc_strings(const char *s, char c, char **split, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	str_size;
+
+	i = 0;
+	j = 0;
+	str_size = 0;
+	while (s[i] && j < size)
+	{
+		str_size = 0;
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i++] != c)
+			str_size++;
+		split[j] = malloc(sizeof(char) * (str_size) + 1);
+		if (split[j] == NULL)
+		{
+			split = NULL;
+			break ;
+		}
+		j++;
+	}
+	split[j] = malloc(sizeof(char));
+}
+
+static int	ft_count_strings(const char *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	str_count;
+
+	i = 0;
+	str_count = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+		{
+			j++;
+			i++;
+		}
+		if (j > 0)
+			str_count += j / j;
+	}
+	return (str_count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	size_t	str_count;
+
+	str_count = ft_count_strings(s, c);
+	split = malloc(sizeof(char *) * (str_count + 1));
+	if (split == NULL)
+		return (NULL);
+	ft_malloc_strings(s, c, split, str_count);
+	ft_fill_strings(s, c, split, str_count);
+	return (split);
+}
