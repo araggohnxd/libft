cda7ebf09fabe823747b50ea9c87560581e68bbc
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-05-17 16:06:15 -0300

    Remove undesired variable

--- sources/string/ft_strndup.c
+++ sources/string/ft_strndup.c
@@ -6,28 +6,20 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:42:58 by maolivei          #+#    #+#             */
-/*   Updated: 2023/04/25 21:39:41 by maolivei         ###   ########.fr       */
+/*   Updated: 2023/05/17 16:02:13 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int loop = 0;
 
 char	*ft_strndup(const char *s, size_t n)
 {
 	const size_t	length = ft_strnlen(s, n);
 	char			*dup;
 
-	if (loop >= 2)
-		return (NULL);
-		
 	dup = (char *)malloc(sizeof(char) * (length + 1));
 	if (!dup)
 		return (NULL);
 	dup[length] = '\0';
-
-	++loop;
-
 	return ((char *)ft_memcpy(dup, s, length));
 }
